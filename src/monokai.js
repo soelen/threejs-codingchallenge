1402eb02fb8e37151e670a475bb07cd61c6694a4
Uwe Sölen Flaig <uwe.soelen.flaig@gmail.com>
2020-03-03 17:56:09 +0100

    Added simple monokai color scheme.

---
+++ src/monokai.js
@@ -0,0 +1,27 @@
+// https://marketplace.visualstudio.com/items?itemName=SuperPaintman.monokai-extended
+
+import { Color } from 'three';
+
+const background = new Color( 0x272822 );
+const foreground = new Color( 0xf8f8f2 );
+const comment = new Color( 0x75715E );
+const red = new Color( 0xf92672 );
+const orange = new Color( 0xfd971f );
+const lightOrange = new Color( 0xe69f66 );
+const yellow = new Color( 0xe6db74 );
+const green	= new Color( 0xa6e22e );
+const blue = new Color( 0x66d9ef );
+const purple = new Color( 0xae81ff );
+
+export {
+    background,
+    foreground,
+    comment,
+    red,
+    orange,
+    lightOrange,
+    yellow,
+    green,
+    blue,
+    purple,
+}
